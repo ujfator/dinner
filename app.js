6a2ae3e05d78c570f80fc40d65ef0ffa6351d194
ujfator <33957024+ujfator@users.noreply.github.com>
2017-12-03 15:30:43 +0100

    buttonas

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function vyberJidla () {
     alert("cus vole");
 };
 vyberJidla();
+
+function pictureRemoval() {
+    document.getElementsByTagName("img")[0].style.display = "none";
+    document.getElementsByTagName("button")[0].style.display = "none";
+}
