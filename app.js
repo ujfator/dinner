2be4f555eca5fc886f281a0fa19f8e5fe3bae234
ujfator <33957024+ujfator@users.noreply.github.com>
2017-12-05 12:56:46 +0100

    pcs put inline, alert changed from onlick from onload

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-function welcome () {
-    alert("Vítejte!");
+function welcome() {
+    alert("Vítejte! Najeďte myší na text.");
 };
-welcome();
 
 function pictureRemoval() {
     document.getElementsByClassName("toHide")[0].style.display = "none";
     document.getElementsByClassName("toShow")[0].style.display = "block";
+    welcome();
 }
-function showPicture0 () {
-    document.getElementById("horiatiki").style.display = "block";
+function showPicture0() {
+    document.getElementById("horiatiki").style.display = "inline-block";
 }
-function showPicture1 () {
-    document.getElementById("wings").style.display = "block";
+function showPicture1() {
+    document.getElementById("wings").style.display = "inline-block";
 }
-function showPicture2 () {
-    document.getElementById("pasta").style.display = "block";
+function showPicture2() {
+    document.getElementById("pasta").style.display = "inline-block";
 }
-function showPicture3 () {
-    document.getElementById("gnocchi").style.display = "block";
+function showPicture3() {
+    document.getElementById("gnocchi").style.display = "inline-block";
 }
